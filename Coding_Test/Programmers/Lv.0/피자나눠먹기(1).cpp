4a278627b50778133801143c88622934210a34ef
Ugeun Ji <84713532+UGeunJi@users.noreply.github.com>
2024-07-11 21:35:41 +0900

    Create 피자나눠먹기(1).cpp

---
+++ Coding_Test/Programmers/Lv.0/피자나눠먹기(1).cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    if (n % 7 == 0) {
+        return n / 7;
+    } else {
+        return n / 7 + 1;
+    }
+}
